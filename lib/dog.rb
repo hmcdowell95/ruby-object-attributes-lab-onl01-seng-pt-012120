bc16f456613af487e6faa6c300f432343f52f320
Harley McDowell <hmcdowell95@gmail.com>
2020-02-04 21:03:00 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog 
+  def initalize(dog_name)
+    @this_dogs_name = dog_name
+  end 
+  
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  
+  def breed=(dog_breed)
+    @this_breed = dog_breed
+  end
+  
+  def breed
+    
+  end
+  
+end
+    
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(persons_name)
+    @this_name = persons_name
+  end
+  
+  def name
+    @this_name
+  end
+  
+  def job=(occupation)
+    @their_job = occupation
+  end
+  
+  def job 
+    @their_job
+  end
+end
