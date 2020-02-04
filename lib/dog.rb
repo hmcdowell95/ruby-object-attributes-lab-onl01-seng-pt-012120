fe624094f85d3c14ec6059f3d83d4b73a0e0c153
Harley McDowell <hmcdowell95@gmail.com>
2020-02-04 21:06:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,22 @@
 class Dog 
-  def initalize(dog_name)
-    @this_dogs_name = dog_name
-  end 
   
   def name=(dog_name)
     @this_dogs_name = dog_name
   end
   
+  def name
+    @this_dogs_name
+  end
   
   def breed=(dog_breed)
     @this_breed = dog_breed
   end
   
   def breed
-    
+    @this_breed
   end
+  
+  dog_name
   
 end
     
