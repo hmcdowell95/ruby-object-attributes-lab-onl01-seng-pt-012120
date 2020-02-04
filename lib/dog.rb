f89a1881ec4e2c9c17c880780a35944d8ac77e69
Harley McDowell <hmcdowell95@gmail.com>
2020-02-04 21:11:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,20 @@
 class Dog 
   
   def name=(dog_name)
-    @this_dogs_name = dog_name
+    @name = dog_name
   end
   
   def name
-    @this_dogs_name
+    @name
   end
   
   def breed=(dog_breed)
-    @this_breed = dog_breed
+    @breed = dog_breed
   end
   
   def breed
-    @this_breed
+    @breed
   end
-  
-  dog_name.instance_variable_set(:@name)
-  dog_breed.instance_variable_set(:@breed)
   
 end
     
