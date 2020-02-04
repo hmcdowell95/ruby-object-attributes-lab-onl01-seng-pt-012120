5ad02e7cc54e8a9cfe2181bb2355324dd6b533f0
Harley McDowell <hmcdowell95@gmail.com>
2020-02-04 21:11:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,17 @@
 class Person
   def name=(persons_name)
-    @this_name = persons_name
+    @name = persons_name
   end
   
   def name
-    @this_name
+    @name
   end
   
   def job=(occupation)
-    @their_job = occupation
+    @job = occupation
   end
   
   def job 
-    @their_job
+    @job
   end
 end
